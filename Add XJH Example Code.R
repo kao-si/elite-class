c9f1d19c904ed3d99e1de0367bd4b1ff5250eb49
Kao Si <mackintoshsikao@gmail.com>
2023-10-04 20:59:51 +0800

    Add XJH template

--- Add XJH Example Code.R
+++ Add XJH Example Code.R
@@ -3,36 +3,13 @@
 # Add XJH Example - Cohort 2010
 
 
-## Step 1: Unique Identifier ====
-
-# For each class number set within each cohort, create an unique identifier
-# (班号_姓名) in each exam file
-
+## Functions ====
 
 # Create a function to facilitate creating unique identifier
 cui <- function(df, bh = "BH", xm = "姓名", nv = "cls_name"){
   df[[nv]] <- paste(df[[bh]], df[[xm]], sep = "_")
   return(df)
 }
-
-# Create unique identifiers in the exam files
-c10_20101110yk <- cui(c10_20101110yk) # Source file (with XJH)
-c10_20110122qm <- cui(c10_20110122qm)
-c10_20110428qz <- cui(c10_20110428qz)
-c10_20110708qm <- cui(c10_20110708qm) # Target file (without XJH)
-c10_20111104qz <- cui(c10_20111104qz, bh = "BH1")
-
-
-## Step 2: Check XJH ====
-
-# Double confirm that XJH is correct across all source files
-
-
-# Gather XJH of all source files
-check1 <- select(c10_20101110yk, cls_name, XJH) %>% 
-  left_join(select(c10_20110122qm, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any") %>%
-  left_join(select(c10_20110428qz, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any") %>% 
-  left_join(select(c10_20111104qz, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any")
 
 # Create a function to perform the check on potential errors in XJH across the source files 
 mis <- function(df){
@@ -52,27 +29,6 @@ mis <- function(df){
   }
 }
 
-# Perform the check using function 'mis'
-mis(check1)
-
-# In this case, turns out that the file 'c10_20110122qm' contains incorrect XJH
-
-# Action: Exclude 'c10_20110122qm' as a valid source file and treat it as another target file,
-# and also update README file
-
-
-# <<<<<< Perform steps 1-2 for each class number set >>>>>> 
-
-
-## Step 3: Add XJH ====
-
-
-# Left_join XJH of all source files to the target file
-c10_20110708qm <- c10_20110708qm %>% 
-  left_join(select(c10_20101110yk, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any") %>%
-  left_join(select(c10_20110428qz, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any") %>% 
-  left_join(select(c10_20111104qz, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any")
-
 # Create a function to replace NAs in XJH extracted from the first source file
 # with values in XJH extracted from all the other source files
 fillna <- function(df, nv = "cls_name"){
@@ -89,17 +45,79 @@ fillna <- function(df, nv = "cls_name"){
   return(df)
 }
 
-# Get the positions of relevant XJH columns for checks and later operations
+# Create a function to replace incorrect XJH in the target file
+# df1 is the target file
+# df2 is the source file
+# ui is the value of the unique identifier
+# xh is the XH of the student with an incorrect XJH
+dup <- function(df1, df2, ui, xh, 
+                xhv1 = "XH", xhv2 = "XH", xjhv1 = "XJH", xjhv2 = "XJH", uiv = "cls_name"){
+  df1[[xjhv1]][df1[[uiv]] == ui & df1[[xhv1]] == xh] <- 
+    df2[[xjhv2]][df2[[uiv]] == ui & df2[[xhv2]] == xh]
+  return(df1)
+}
+
+
+################################################################################
+# <<<<<< Perform Step 1 & 2 for each *class number set* >>>>>>
+
+
+## Step 1: Unique Identifier ====
+
+# For each class number set within each cohort, create an unique identifier
+# (班号_姓名) in each exam file
+
+# Create unique identifiers in the exam files
+c10_20101110yk <- cui(c10_20101110yk) # Source file (with XJH)
+c10_20110122qm <- cui(c10_20110122qm)
+c10_20110428qz <- cui(c10_20110428qz)
+c10_20110708qm <- cui(c10_20110708qm) # Target file (without XJH)
+c10_20111104qz <- cui(c10_20111104qz, bh = "BH1")
+
+## Step 2: Check XJH ====
+## >>> DO NOT SAVE THIS SECTION IN R SCRIPT 
+
+# Double confirm that XJH is correct across all source files
+
+# Gather XJH of all source files
+check1 <- select(c10_20101110yk, cls_name, XJH) %>% 
+  left_join(select(c10_20110122qm, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any") %>%
+  left_join(select(c10_20110428qz, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any") %>% 
+  left_join(select(c10_20111104qz, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any")
+
+# Perform the check using function 'mis'
+mis(check1)
+
+# In this case, turns out that the file "c10_20110122qm" contains incorrect XJH
+
+## ACTIONS:
+## 1. Exclude "c10_20110122qm" as a valid source file and treat it as another target file to be processed
+## 2. Update README file (add "$" before "c10_20110122qm")
+
+
+################################################################################
+# <<<<<< Perform Step 3 - 5 for each *target file* >>>>>>
+
+
+## Step 3: Add XJH ====
+
+# Left_join XJH of all source files to the target file
+c10_20110708qm <- c10_20110708qm %>% 
+  left_join(select(c10_20101110yk, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any") %>%
+  left_join(select(c10_20110428qz, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any") %>% 
+  left_join(select(c10_20111104qz, cls_name, XJH), by = "cls_name", na_matches = "never", multiple = "any")
+
+# >>> Get the positions of relevant XJH columns for checks and later operations
 # To return column names, use 'value = TRUE'
 grep("XJH", colnames(c10_20110708qm))
 
-# Check number of NAs in XJH extracted from the first source file before replacement
+# >>> Check number of NAs in XJH extracted from the first source file before replacement
 sum(is.na(c10_20110708qm[84]))
 
 # Perform the replacement using function 'fillna'
 c10_20110708qm <- fillna(c10_20110708qm)
 
-# Check number of NAs in XJH extracted from the first source file after replacement
+# >>> Check number of NAs in XJH extracted from the first source file after replacement
 sum(is.na(c10_20110708qm[84]))
 
 # Rename the focal XJH column
@@ -108,9 +126,7 @@ colnames(c10_20110708qm)[84] <- "XJH"
 # Remove the other XJH columns
 c10_20110708qm[c(2, 85, 86)] <- NULL
 
-
 ## Step 4: Duplicate Values in Unique Identifier ====
-
 
 # Check for duplicate values in "cls_name"
 c10_20110708qm %>% count(cls_name) %>% filter(n > 1)
@@ -122,32 +138,16 @@ c10_20110708qm %>% filter(cls_name == "26_张帅") %>% select(cls_name, XH, XJH)
 
 c10_20101110yk %>% filter(cls_name == "26_张帅") %>% select(cls_name, XH, XJH)
 
-# Create a function to replace incorrect XJH in the target file
-# df1 is the target file
-# df2 is the source file
-# ui is the value of the unique identifier
-# xh is the XH of the student with an incorrect XJH
-dup <- function(df1, df2, ui, xh, xhv = "XH", uiv = "cls_name", xjhv1 = "XJH", xjhv2 = "XJH"){
-  df1[[xjhv1]][df1[[uiv]] == ui & df1[[xhv]] == xh] <- 
-    df2[[xjhv2]][df2[[uiv]] == ui & df2[[xhv]] == xh]
-  return(df1)
-}
-
 # Perform the replacement using function 'dup'
 c10_20110708qm <- dup(c10_20110708qm, c10_20101110yk, "26_张帅", 33)
 
-# Double check to confirm the change
+# >>> Double check to confirm the change
 c10_20110708qm %>% filter(cls_name == "26_张帅") %>% select(cls_name, XH, XJH)
 
-
 ## Step 5: Check the Completed Target File ====
-
-
+## >>> DO NOT SAVE THIS SECTION IN R SCRIPT
+ 
 c10_20110708qm %>% select(cls_name, XJH, 1:15) %>%
   group_by(BH) %>% 
   slice(1:5) %>% 
   print(n = Inf)
-
-
-# <<<<<< Perform steps 3-5 for each target file >>>>>>
-
---
+++ Elite-Class_02-Add-Student-Status-ID-(XJH).R
@@ -0,0 +1,78 @@
+
+
+library(tidyverse)
+
+
+# Functions ####
+
+
+# Function that facilitates the creation of unique identifier
+cui <- function(df, bh = "BH", xm = "姓名", nv = "cls_name"){
+  df[[nv]] <- paste(df[[bh]], df[[xm]], sep = "_")
+  return(df)
+}
+
+# Function that performs checks on potential errors in XJH across the source files 
+mis <- function(df){
+  
+  # create the output vector
+  mismatch <- numeric(length = ncol(df) - 2)
+  
+  for(i in 3:ncol(df)){
+    
+    for(j in 1:length(mismatch)){
+      
+      # get the number of mismatches between XJH of the first source file and XJH of the other source files
+      mismatch[[j]] <- which(df[[2]] != df[[i]]) %>% length()
+      
+      return(mismatch)
+    }
+  }
+}
+
+# Function that replaces NAs in XJH extracted from the first source file
+# with values in XJH extracted from all the other source files
+fillna <- function(df, nv = "cls_name"){
+  
+  # n is the position of "cls_name" column
+  n <- which(colnames(df) == nv)
+  
+  # n + 1 is the position of XJH extracted from the first source file
+  # n + 2 is the position of XJH extracted from the second source file
+  for(i in (n + 2):ncol(df)){
+    usecol <- df[[i]]
+    df[[n + 1]][which(is.na(df[[n + 1]]))] <- usecol[which(is.na(df[[n + 1]]))]
+  }
+  return(df)
+}
+
+# Function that replaces incorrect XJH in the target file that has
+# duplicate values of unique identifier
+
+# df1 is the target file
+# df2 is the source file
+# ui is the value of the unique identifier
+# xh is the XH of the student with an incorrect XJH
+dup <- function(df1, df2, ui, xh, 
+                xhv1 = "XH", xhv2 = "XH", xjhv1 = "XJH", xjhv2 = "XJH", uiv = "cls_name"){
+  df1[[xjhv1]][df1[[uiv]] == ui & df1[[xhv1]] == xh] <- 
+    df2[[xjhv2]][df2[[uiv]] == ui & df2[[xhv2]] == xh]
+  return(df1)
+}
+
+
+# Cohort 2010 ####
+
+
+## Class Number Set 1 ====
+
+
+### Create Unique Identifier in Every Exam Files ----
+
+### >>Add XJH to **c10_20110122qm** ----
+
+#### Cases with Duplicate Values of Unique Identifier ----
+
+### >>Add XJH to **c10_20110708qm** ----
+
+#### Cases with Duplicate Values of Unique Identifier ----
