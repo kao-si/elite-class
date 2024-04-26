d4ad47ad401b00f54cd6b463a2897a9ecf7c9283
Kao Si <mackintoshsikao@gmail.com>
2024-04-26 14:47:33 +0800

    Update analysis
    Class number set

--- Elite-Class_04_Analysis.R
+++ Elite-Class_04_Analysis.R
@@ -31,7 +31,64 @@ pivot_wider(
   names_repair = "check_unique"
 )
 
-## Zeros in score variables ====
+## Class number set ====
+
+# Create "cls_set" variable for each cohort
+data <- data %>%
+  mutate(
+    cls_set = case_when(
+      # cohorts 2003, 2004, 2007, and 2010 - 2014
+      cohort %in% c("2003", "2004", "2007", "2010", "2011", "2012", "2013", "2014") &
+        str_detect(exam, "hsee|^g1") ~ "1",
+      cohort %in% c("2003", "2004", "2007", "2010", "2011", "2012", "2013", "2014") &
+        !str_detect(exam, "hsee|^g1") ~ "2",
+      # cohort 2005
+      cohort == "2005" & exam %in% c("hsee", "g1m1", "g1f1") ~ "1",
+      cohort == "2005" & !exam %in% c("hsee", "g1m1", "g1f1") ~ "2",
+      # cohort 2006
+      cohort == "2006" & exam %in% c("hsee", "g1m1", "g1f1") ~ "1",
+      cohort == "2006" & exam %in% c("g1m2", "g1f2") ~ "2",
+      cohort == "2006" & !str_detect(exam, "hsee|^g1") ~ "3",
+      # cohort 2008
+      cohort == "2008" & str_detect(exam, "hsee|^g1") ~ "1",
+      cohort == "2008" & str_detect(exam, "^g2") ~ "2",
+      cohort == "2008" & str_detect(exam, "^g3|cee") ~ "3",
+      # cohort 2009
+      cohort == "2009" & str_detect(exam, "hsee|^g1") ~ "1",
+      cohort == "2009" & exam %in% c("g2m1", "g2f1") ~ "2",
+      cohort == "2009" & str_detect(exam, "g2m2|g2f2|^g3|cee") ~ "3"
+    )
+  )
+
+# Tidy "trk", "cls", and "cid"
+data <- data %>%
+  mutate(
+    trk = case_when(
+      str_detect(trk, "L|l|普理|艺理|体育|理") ~ "Science Track",
+      str_detect(trk, "W|w|普文|艺文|文") ~ "Liberal Arts Track",
+      TRUE ~ NA
+    ),
+    cls = case_when(
+      cls == "0" ~ NA,
+      str_detect(cls, "^0") ~ substr(cls, 2, 2),
+      str_detect(cls, "\\D") ~ NA,
+      TRUE ~ cls
+    ),
+    cid = case_when(
+      cid == "0" ~ NA,
+      cid == "99" ~ NA,
+      str_detect(cid, "^0") ~ substr(cid, 2, 2),
+      TRUE ~ cid
+    )
+  )
+
+# Fill missing values of "trk", "cls", and "cid"
+data <- data %>%
+  group_by(cssid, cls_set) %>%
+  fill(trk, cls, cid, .direction = "updown") %>%
+  ungroup()
+
+## Zeros in exam scores ====
 
 # First create a vector containing the variable names
 score_col <- c("tot", "chn", "mat", "eng", "phy", "che", "bio", "geo",
@@ -43,11 +100,11 @@ data[score_col] <- map_df(data[score_col], as.numeric)
 # Replace negative values and 0s with NA
 data[score_col][data[score_col] <= 0] <- NA
 
-## Track variable ====
+## Study track ====
 
 # Identify students in science track using their exam scores in Grade 3
 dat_sci <- data %>% 
-  filter(exam %in% c("g2f2", "g3m1", "g3f1", "g3k1", "g3k2", "cee")) %>%
+  filter(exam %in% c("g3m1", "g3f1", "g3k1", "g3k2", "cee")) %>%
   filter(
     !is.na(sci) | (!is.na(phy) & !is.na(che) & !is.na(bio))
   ) %>% 
@@ -57,7 +114,7 @@ id_sci <- unique(dat_sci$cssid)
 
 # Identify students in liberal arts track using their exam scores in Grade 3
 dat_lib <- data %>% 
-  filter(exam %in% c("g2f2", "g3m1", "g3f1", "g3k1", "g3k2", "cee")) %>%
+  filter(exam %in% c("g3m1", "g3f1", "g3k1", "g3k2", "cee")) %>%
   filter(
     !is.na(lib) | (!is.na(pol) & !is.na(his) & !is.na(geo))
   ) %>% 
@@ -76,20 +133,9 @@ data <- data %>%
   )
 
 # Continue with "trk" variable for students who remain unidentified
-# in the above step
-
-# First tidy "trk"
-data <- data %>%
-  mutate(
-    trk = case_when(
-      str_detect(trk, "L|l|普理|艺理|体育|理") ~ "Science Track",
-      str_detect(trk, "W|w|普文|艺文|文") ~ "Liberal Arts Track",
-      TRUE ~ NA
-    )
-  )
-
-# For students who have at least one non-NA value in "trk", we use
-# the last non-NA value in "trk"
+# in the above step; 
+# For students who have non-NA values in "trk", we use the last
+# non-NA value in "trk"
 data <- data %>% 
   group_by(cssid) %>%
   mutate(
@@ -109,44 +155,35 @@ data <- data %>%
     )
   )
 
-## Class number variable (wip) ====
-
-# First tidy "cls" and also "cid"
+## Comprehensive test scores ====
 data <- data %>%
-  mutate(
-    cls = case_when(
-      cls == "0" ~ NA,
-      str_detect(cls, "^0") ~ substr(cls, 2, 2),
-      str_detect(cls, "\\D") ~ NA,
-      TRUE ~ cls
-    ),
-    cid = case_when(
-      cid == "0" ~ NA,
-      str_detect(cid, "^0") ~ substr(cid, 2, 2),
-      TRUE ~ cid
-    )
-  )
-
-## Comprehensive test scores (wip) ====
-# >>>>>> deal with "com" 
-data <- data %>%
+  # when "sci" or "lib" is NA, first replace with "com"
   mutate(
     sci = case_when(
-      # when "sci" is NA, replace with the sum of scores of
-      # the three composing subjects
-      is.na(sci) ~ rowSums(select(., phy, che, bio), na.rm = TRUE),
+      is.na(sci) & track == "Science Track" ~ com,
       TRUE ~ sci
     ),
     lib = case_when(
-      # when "lib" is NA, replace with the sum of scores of
-      # the three composing subjects
-      is.na(lib) ~ rowSums(select(., geo, his, pol), na.rm = TRUE),
+      is.na(lib) & track == "Liberal Arts Track" ~ com,
       TRUE ~ lib
     )
-  ) 
+  ) %>% 
+  # continue to replace NAs of "sci" and "lib" with the sum of scores of
+  # the three composing subjects (only when all three subjects have scores)
+  mutate(
+    sci = case_when(
+      is.na(sci) ~ rowSums(select(., phy, che, bio), na.rm = FALSE),
+      TRUE ~ sci
+    ),
+    lib = case_when(
+      is.na(lib) ~ rowSums(select(., geo, his, pol), na.rm = FALSE),
+      TRUE ~ lib
+    )
+  )
 
-# Replace 0s produced in the above step with NA
-data[score_col][data[score_col] == 0] <- NA
+## Anomalous exam scores (wip) ====
+
+## Elite class (wip) ====
 
 ## Standardize exam scores within cohort/exam (wip) ====
 # >>>>>> standardize "tot" around the cutoffs
